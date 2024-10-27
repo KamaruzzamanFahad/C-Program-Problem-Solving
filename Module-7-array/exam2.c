e637a2df35d2b33102feb70c2c80b2c98c2a1b70
Kamaruzzaman Fahad <58844727+KamaruzzamanFahad@users.noreply.github.com>
2024-10-27 20:22:28 +0600

    Update exam2.c

--- Module-7-array/exam2.c
+++ Module-7-array/exam2.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
     //Babul's Vowels problem colve in c progream 
-    char ch;
+    char ch ;
     scanf("%c", &ch);
     if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
         printf("Vowel");
